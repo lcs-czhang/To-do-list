c1fbf154f089d6c0daeb89efa9368f5761a2068f
Youzhou Zhang <czhang@lcs.on.ca>
2024-04-08 12:04:05 -0400

    Initial Commit

---
+++ To-do list/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-do list
+//
+//  Created by Yuzhou Zhang on 2024-04-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To-do list
+//
+//  Created by Yuzhou Zhang on 2024-04-08.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
